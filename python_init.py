b03ed6e8e6b51efafba9576cff37bebf2275e8a5
Anthony Ho <anthony.hyho@gmail.com>
2015-01-29 13:36:05 -0800

    Adds python_init.py file to import modules at start

---
+++ python_init.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python 
+# Python startup script to load the relevant modules
+# Last update AH 1/29/2015
+
+import os, sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotlib
