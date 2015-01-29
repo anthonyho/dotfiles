59e3bf5480a569d12443bbd5ff4963c685ac1d4e
Anthony Ho <anthony.hyho@gmail.com>
2015-01-28 19:13:05 -0800

    Fixes python path

--- bashrc_common.bash
+++ bashrc_common.bash
@@ -14,7 +14,7 @@ export HISTFILESIZE=1000  # Changing history size
 export HISTCONTROL='ignoredups'  # Eliminate duplicates in history
 export EDITOR='emacs -nw'  # Set default edit to emacs
 export PATH="$PATH:$HOME/scripts/linuxTools:$HOME/scripts/AHH_arrayTools/quantTools:$HOME/scripts/AHH_arrayTools/seqTools:$HOME/scripts/AHH_arrayTools/miscell"
-export PYTHONPATH="$PYTHONPATH:$HOME/AHH_arrayTools/pythonLib"
+export PYTHONPATH="$PYTHONPATH:$HOME/scripts/AHH_arrayTools/pythonLib"
 if [ "$(uname)" == "Darwin" ]; then  # Mac
     export LSCOLORS=GxFxCxDxBxegedabagaced  # Specify ls colors for Mac
 fi
