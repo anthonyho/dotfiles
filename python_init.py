f8f4f0e54b521d3bc12a1b4c3460df7027945cb2
Anthony Ho <anthony.hyho@gmail.com>
2015-02-11 15:59:47 -0800

    import more modules at ipython startup

--- python_init.py
+++ python_init.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python 
 # Python startup script to load the relevant modules
-# Last update AH 1/29/2015
+# Last update AH 2/11/2015
 
 import os, sys
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import plotlib
+import fitlib
+import parselib
+from joblib import Parallel, delayed
+import multiprocessing
