31be0799f042b0579df6321f9c3aba7037b38916
Anthony Ho <anthony.hyho@gmail.com>
2016-04-08 10:17:20 -0700

    add aliases on xstream

--- bashrc_xs.bash
+++ bashrc_xs.bash
@@ -1,5 +1,11 @@
 # .bashrc for xStream
 # Last update AH 4/1/2015
 
-# Load the appropriate modules
+# Define aliases
 module load foss git R RStudio
+
+alias cdw='cd $GROUP_WORK'
+alias ml='module load foss git RStudio'
+alias mp='module purge; rm ~/.lmod.d/.cache/*'
+alias rs='rstudio &'
+alias sr='function _sr(){ srun -N1 -c$* --x11 --pty bash -l; }; _sr'
