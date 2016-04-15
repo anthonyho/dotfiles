26d14128f74efbde70ba1bf0ef83c36189b5bb4c
Anthony Ho <anthony.hyho@gmail.com>
2016-04-15 13:35:51 -0700

    add MATLAB startup file for pegasus

---
+++ startup_pegasus.m
@@ -0,0 +1,13 @@
+%STARTUP   Startup file
+%   Change the name of this file to STARTUP.M. The file 
+%   is executed when MATLAB starts up, if it exists 
+%   anywhere on the path.  In this example, the
+%   MAT-file generated during quitting using FINISHSAV
+%   is loaded into MATLAB during startup.
+
+%   Copyright 1984-2000 The MathWorks, Inc. 
+%   $Revision: 1.4 $  $Date: 2000/06/01 16:19:26 $
+
+addpath('/Users/anthony/scripts/arrayAnalysisTools/matlabLib');
+addpath('/Users/anthony/scripts/arrayAnalysisTools/old/matlab');
+addpath('/Users/anthony/scripts/arrayAnalysisTools/old/matlab/fitFunctions');
