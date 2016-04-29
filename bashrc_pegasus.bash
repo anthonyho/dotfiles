31a6f492480a969f3ebe66e8ec59b6719e733228
Anthony Ho <anthony.hyho@gmail.com>
2016-04-28 18:41:09 -0700

    add local bashrc file for pegasus

---
+++ bashrc_pegasus.bash
@@ -0,0 +1,11 @@
+# .bashrc for Laurel
+# Last update AH 4/28/2016
+
+# --- Environmental variables --- #
+#export VMDAPPDIR='/Applications/VMD1.9.1.app/Contents'
+export PATH="$PATH:/Applications/MATLAB_R2015b.app/bin"
+
+# --- Define aliases --- #
+#alias vmd='"$VMDAPPDIR/Resources/VMD.app/Contents/MacOS/VMD" $* -nt'
+#alias vmdd='"$VMDAPPDIR/Resources/VMD.app/Contents/MacOS/VMD" $* -dispdev text -e'
+#alias xmgrace='/Applications/Grace.app/Contents/MacOS/start.sh'
