8feb24682213cf4113c24f82eaf59e7cd8e02dfc
Anthony Ho <anthony.hyho@gmail.com>
2016-05-04 18:36:13 -0700

    add protobuf for module load

--- bashrc_xs.bash
+++ bashrc_xs.bash
@@ -1,11 +1,11 @@
 # .bashrc for xStream
-# Last update AH 4/1/2015
+# Last update AH 5/4/2015
 
 # Define aliases
 #module load foss git R RStudio
 
 alias cdw='cd $GROUP_WORK'
-alias ml='module load foss git RStudio'
+alias ml='module load foss git protobuf/2.4.1 RStudio'
 alias mp='module purge; rm ~/.lmod.d/.cache/*'
 alias rs='rstudio &'
 alias sr='function _sr(){ srun -N1 -c$* --x11 --pty bash -l; }; _sr'
