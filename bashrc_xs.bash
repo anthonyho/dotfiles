82da4b84727ecfc84ee4f957aa893aa0eeb41772
Anthony Ho <anthony.hyho@gmail.com>
2016-05-12 16:46:49 -0700

    add R to module load path

--- bashrc_xs.bash
+++ bashrc_xs.bash
@@ -5,7 +5,7 @@
 #module load foss git R RStudio
 
 alias cdw='cd $GROUP_WORK'
-alias ml='module load foss git protobuf/2.4.1 RStudio'
+alias ml='module load foss git protobuf/2.4.1 R RStudio'
 alias mp='module purge; rm ~/.lmod.d/.cache/*'
 alias rs='rstudio &'
 alias sr='function _sr(){ srun -N1 -c$* --x11 --pty bash -l; }; _sr'
