42577eed71fc1558f06ebc02db052e428a4b8833
Anthony Hung Yu Ho <ahho@sherlock-ln01.stanford.edu>
2016-06-29 14:44:55 -0700

    add bashrc for sherlock

---
+++ bashrc_sherlock.bash
@@ -0,0 +1,5 @@
+# .bashrc for Sherlock
+# Last update AH 6/29/2016
+
+# --- Environmental variables --- #
+export MATLABPATH="$HOME/scripts/array_tools:$HOME/scripts/array_tools/CPlibs:$HOME/scripts/array_tools/CPscripts:$MATLABPATH"
