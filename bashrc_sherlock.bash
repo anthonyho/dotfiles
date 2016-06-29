fe0e824a590721924b064167993d378c9747a880
Anthony Hung Yu Ho <ahho@sherlock-ln03.stanford.edu>
2016-06-29 15:36:33 -0700

    mask library import in python startup for sherlock

--- bashrc_sherlock.bash
+++ bashrc_sherlock.bash
@@ -2,4 +2,5 @@
 # Last update AH 6/29/2016
 
 # --- Environmental variables --- #
+export PYTHONSTARTUP="$HOME/scripts/dotfiles/python_init_sherlock.py"
 export MATLABPATH="$HOME/scripts/array_tools:$HOME/scripts/array_tools/CPlibs:$HOME/scripts/array_tools/CPscripts"
---
+++ python_init_sherlock.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python 
+# Python startup script to load the relevant modules on Sherlock
+# Last update AH 6/29/2016
+
+import os, sys
+from joblib import Parallel, delayed
+import multiprocessing
+
+import numpy as np
+#import pandas as pd
+
+#import matplotlib.pyplot as plt
+
+#import plotlib
+#import fitlib
+#import fitfuns
+#import parselib
+#import seqlib
+#import liblib
+#import varlib
