7fbf2aee07cbc1d9a49e27845973a2083d56665e
Anthony Hung Yu Ho <ahho@sh-5-35.local>
2016-06-29 15:19:19 -0700

    fix problem with logging into interactive node

--- bashrc_sherlock.bash
+++ bashrc_sherlock.bash
@@ -2,4 +2,4 @@
 # Last update AH 6/29/2016
 
 # --- Environmental variables --- #
-export MATLABPATH="$HOME/scripts/array_tools:$HOME/scripts/array_tools/CPlibs:$HOME/scripts/array_tools/CPscripts:$MATLABPATH"
+export MATLABPATH="$HOME/scripts/array_tools:$HOME/scripts/array_tools/CPlibs:$HOME/scripts/array_tools/CPscripts"
