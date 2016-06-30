9ca7adcf09a77867d7519a355efa8715faa5c203
Anthony Ho <anthony.hyho@gmail.com>
2016-06-29 18:50:13 -0700

    change default library in python

--- python_init_sherlock.py
+++ python_init_sherlock.py
@@ -2,11 +2,11 @@
 # Python startup script to load the relevant modules on Sherlock
 # Last update AH 6/29/2016
 
-import os, sys
+#import os, sys
 #from joblib import Parallel, delayed
-import multiprocessing
+#import multiprocessing
 
-import numpy as np
+#import numpy as np
 #import pandas as pd
 
 #import matplotlib.pyplot as plt
