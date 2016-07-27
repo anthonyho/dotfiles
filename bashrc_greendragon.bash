569321a880d4e142c1855fd0c4976e4005e97ab1
Anthony Ho <anthony.hyho@gmail.com>
2016-07-27 14:00:01 -0700

    add GreenDragon specific bashrc file for backup

---
+++ bashrc_greendragon.bash
@@ -0,0 +1,6 @@
+# .bashrc for GreenDragon 
+# Last update AH 7/27/2016
+
+# --- Backup --- #
+alias backupSequencing='rsync -avzP /mnt/raid1/lab/anthony/analysis/sequencing/ $greennas:~/analysis/sequencing/'
+alias backupImaging='rsync -avzP /mnt/raid1/lab/anthony/analysis/imaging/ $greennas:~/analysis/imaging/'
