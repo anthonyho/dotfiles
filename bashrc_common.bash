6d75fbca6ba5976bfd1ef17b3cb3cdc8ccd2e25a
Anthony Ho <anthony.hyho@gmail.com>
2016-08-17 11:25:26 -0700

    add python path to array_fitting_tools/bin

--- bashrc_common.bash
+++ bashrc_common.bash
@@ -13,8 +13,8 @@ export HISTSIZE=1000  # Changing history size
 export HISTFILESIZE=1000  # Changing history size
 export HISTCONTROL='ignoredups'  # Eliminate duplicates in history
 export EDITOR='emacs -nw'  # Set default edit to emacs
-export PATH="$PATH:$HOME/scripts/linuxTools:$HOME/scripts/arrayAnalysisTools/quantTools:$HOME/scripts/arrayAnalysisTools/seqTools:$HOME/scripts/arrayAnalysisTools/miscell"
-export PYTHONPATH="$PYTHONPATH:$HOME/scripts/arrayAnalysisTools/pythonLib:$HOME/scripts/arrayAnalysisTools/projectLib"
+export PATH="$PATH:$HOME/scripts/linuxTools:$HOME/scripts/arrayAnalysisTools/quantTools:$HOME/scripts/arrayAnalysisTools/seqTools:$HOME/scripts/arrayAnalysisTools/miscell:$HOME/scripts/array_fitting_tools/bin"
+export PYTHONPATH="$PYTHONPATH:$HOME/scripts/arrayAnalysisTools/pythonLib:$HOME/scripts/arrayAnalysisTools/projectLib:$HOME/scripts/array_fitting_tools/bin"
 export PYTHONSTARTUP="$HOME/scripts/dotfiles/python_init.py"
 if [ "$(uname)" == "Darwin" ]; then  # Mac
     export LSCOLORS=GxFxCxDxBxegedabagaced  # Specify ls colors for Mac
