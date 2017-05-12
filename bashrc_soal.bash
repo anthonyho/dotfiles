9d5fce02694645bf7e690518269dd53054d1ba33
Anthony Ho <anthony.hyho@gmail.com>
2017-05-12 10:29:18 -0700

    add python startup path and config file for SOAL clusters

---
+++ bashrc_soal.bash
@@ -0,0 +1,5 @@
+# .bashrc for SOAL
+# Last update AH 5/12/2017
+
+# --- Environmental variables --- #
+export PYTHONSTARTUP="$HOME/scripts/dotfiles/python_init_soal.py"
---
+++ python_init_soal.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python 
+# Python startup script to load the relevant modules
+# Last update AH 2/11/2015
+
+#import os, sys
+#from joblib import Parallel, delayed
+#import multiprocessing
+
+#import numpy as np
+#import pandas as pd
+
+#import matplotlib.pyplot as plt
+
+#import plotlib
+#import fitlib
+#import fitfuns
+#import parselib
+#import seqlib
+#import liblib
+#import varlib
